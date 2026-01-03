ffc9b148fe3f357de85fe06addde6c49bd486fe9
PlutonianProgrammer <plutonianprogrammer@gmail.com>
2026-01-03 15:42:56 -0600

    54. Spiral Matrix

--- Solution.py
+++ Solution.py
@@ -1,19 +1,49 @@
 class Solution(object):
-    def rotate(self, matrix):
+    def spiralOrder(self, matrix):
         """
         :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        :rtype: List[int]
         """
-        # Transpose Matrix
-        for y in range(len(matrix)):
-            for x in range(len(matrix[0])):
-                if y < x:
-                    # Swap each element above main diagonal with respective element below
-                    matrix[y][x], matrix[x][y] = matrix[x][y], matrix[y][x]
+        min_x, max_x = 0, len(matrix[0])
+        min_y, max_y = 0, len(matrix)
 
-        # Reverse Rows
-        for i in range(len(matrix)):
-            matrix[i] = matrix[i][::-1]
+        result = []
 
+        x, y = 0, 0
+        mode = 0
+        # mode 0: right
+        # mode 1: down
+        # mode 2: left
+        # mode 3: up
 
-print('test')
+        while min_x < max_x and min_y < max_y:
+            result.append(matrix[y][x])
+            if mode == 0:
+                if x + 1 == max_x:
+                    mode = 1
+                    y += 1
+                    min_y += 1
+                else:
+                    x += 1
+            elif mode == 1:
+                if y + 1 == max_y:
+                    mode = 2
+                    x -= 1
+                    max_x -= 1
+                else:
+                    y += 1
+            elif mode == 2:
+                if x == min_x:
+                    mode = 3
+                    y -= 1
+                    max_y -= 1
+                else:
+                    x -= 1
+            else: # mode == 3
+                if y == min_y:
+                    mode = 0
+                    x += 1
+                    min_x += 1
+                else:
+                    y -= 1
+        return result
