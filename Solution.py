8b9b6e3e3a3c5227fea4817885742943ed3bbb5f
PlutonianProgrammer <plutonianprogrammer@gmail.com>
2026-01-04 10:30:26 -0600

    547. Number of Provinces

--- Solution.py
+++ Solution.py
@@ -1,49 +1,25 @@
 class Solution(object):
-    def spiralOrder(self, matrix):
+    def findCircleNum(self, isConnected):
         """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
+        :type isConnected: List[List[int]]
+        :rtype: int
         """
-        min_x, max_x = 0, len(matrix[0])
-        min_y, max_y = 0, len(matrix)
+        
+        num_provinces = 0
+        explored = set()
 
-        result = []
+        def dfs(node):
+            explored.add(node)
+            for other_node in range(len(isConnected[node])):
+                if isConnected[node][other_node] != 0 and other_node not in explored:
+                    dfs(other_node)
 
-        x, y = 0, 0
-        mode = 0
-        # mode 0: right
-        # mode 1: down
-        # mode 2: left
-        # mode 3: up
-
-        while min_x < max_x and min_y < max_y:
-            result.append(matrix[y][x])
-            if mode == 0:
-                if x + 1 == max_x:
-                    mode = 1
-                    y += 1
-                    min_y += 1
-                else:
-                    x += 1
-            elif mode == 1:
-                if y + 1 == max_y:
-                    mode = 2
-                    x -= 1
-                    max_x -= 1
-                else:
-                    y += 1
-            elif mode == 2:
-                if x == min_x:
-                    mode = 3
-                    y -= 1
-                    max_y -= 1
-                else:
-                    x -= 1
-            else: # mode == 3
-                if y == min_y:
-                    mode = 0
-                    x += 1
-                    min_x += 1
-                else:
-                    y -= 1
-        return result
+        for node in range(len(isConnected)):
+            if node not in explored:
+                dfs(node)
+                num_provinces += 1
+        
+        return num_provinces
+    
+matrix = [[1,0,0],[0,1,0],[0,0,1]]
+print(Solution().findCircleNum(matrix))
