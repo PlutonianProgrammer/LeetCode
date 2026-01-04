ca40f34122221ac7b257d32f917e9547b7817859
PlutonianProgrammer <plutonianprogrammer@gmail.com>
2026-01-04 13:19:53 -0600

    997. Find the Town Judge

--- Solution.py
+++ Solution.py
@@ -1,5 +1,4 @@
 class Solution(object):
-<<<<<<< HEAD
     def findJudge(self, n, trust):
         """
         :type n: int
@@ -30,33 +29,3 @@ class Solution(object):
             return candidates[0] + 1
         else:
             return -1
-
-n = 4
-trust = [[1,3],[1,4],[2,3]]
-print(Solution().findJudge(n, trust))
-=======
-    def findCircleNum(self, isConnected):
-        """
-        :type isConnected: List[List[int]]
-        :rtype: int
-        """
-        
-        num_provinces = 0
-        explored = set()
-
-        def dfs(node):
-            explored.add(node)
-            for other_node in range(len(isConnected[node])):
-                if isConnected[node][other_node] != 0 and other_node not in explored:
-                    dfs(other_node)
-
-        for node in range(len(isConnected)):
-            if node not in explored:
-                dfs(node)
-                num_provinces += 1
-        
-        return num_provinces
-    
-matrix = [[1,0,0],[0,1,0],[0,0,1]]
-print(Solution().findCircleNum(matrix))
->>>>>>> 8b9b6e3e3a3c5227fea4817885742943ed3bbb5f
